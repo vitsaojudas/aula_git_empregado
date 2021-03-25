62083e12a5333c6b4d02d95a0a98d51a38b50c8b
Vitoria Alves Salata <vitoriasalata.3861@aluno.saojudas.br>
2021-03-24 22:45:51 -0300

    Mais modificações

--- Empregado.java
+++ Empregado.java
@@ -43,4 +43,29 @@ public class Empregado {
     public int tipo() {
         return this.tipo;
     }
+
+    // adicionar à classe Empregado.java
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getSalario() {
+        return this.salario;
+    }
+
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+
+    public double getComissao() {
+        return this.comissao;
+    }
+
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+
+    public double getBonus() {
+        return this.bonus;
+    }
 }
--- TesteEmpregado.java
+++ TesteEmpregado.java
@@ -1,9 +1,22 @@
+//a classe TesteEmpregado ficou assim
 public class TesteEmpregado {
-
     public static void main(String[] args) {
         Empregado e1 = new Empregado();
         Empregado e2 = new Empregado();
         Empregado e3 = new Empregado();
-        // e agora, como configurar o tipo deles?
+        // configurando tipo dos empregados
+        e1.setTipo(1);
+        e2.setTipo(2);
+        e3.setTipo(3);
+        // configurando alguns valores
+        e1.setSalario(2000);
+        e2.setSalario(1700);
+        e2.setComissao(0.2);// 20% de comissão sobre o salário
+        e3.setSalario(1500);
+        e3.setBonus(400);
+        // mostra os valores
+        System.out.println(e1.calculaSalario());
+        System.out.println(e2.calculaSalario());
+        System.out.println(e3.calculaSalario());
     }
 }
