cd7cbecbe4491366cd8b7ee9bdd0cbbf9635f18f
Vitoria Alves Salata <vitoriasalata.3861@aluno.saojudas.br>
2021-03-24 22:06:34 -0300

    Primeira modificção

--- Empregado.java
+++ Empregado.java
@@ -1,19 +1,37 @@
-public class Empregado{
-private String nome;
-private int idade;
-public String getNome (){
-return this.nome;
-}
+public class Empregado {
+    private String nome;
+    private int idade;
+    private int tipo;
+    private double salario;
+    private double comissao;
+    private double bonus;
 
-public void setNome (String nome){
-this.nome = nome;
-}
+    public double calculaSalario() {
+        if (tipo == 1) {
+            return salario;
+        } else if (tipo == 2) {
+            // comissão varia de 0 a 1, é um percentual sobre o salário
+            return salario + salario * comissao;
+        } else if (tipo == 3) {
+            return salario + bonus;
+        } else {
+            return 0.;
+        }
+    }
 
-public int getIdade (){
-return this.idade;
-}
+    public String getNome() {
+        return this.nome;
+    }
 
-public void setIdade (int idade){
-this.idade = idade;
-}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
